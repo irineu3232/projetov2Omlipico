f0c1bb3bad3032a955ae6a1ec6cb7ef31235f14b
Paulo <irineu32323@gmail.com>
2025-09-02 09:50:46 -0300

    4- Views de procurar modalidade e procurar por estado realizadas

--- Projeto-Olimpiadas-main/POlimpicos/Controllers/ModalidadesController.cs
+++ Projeto-Olimpiadas-main/POlimpicos/Controllers/ModalidadesController.cs
@@ -11,7 +11,32 @@ namespace POlimpicos.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var lista = new List<Modalidades>();
+
+            using (var conn = db.GetConnection())
+            using (var cmd = new MySqlCommand(@"
+                        Select Distinct
+                        m.codModalidade as modalidade, m.nomeModalidade as nome
+                        From Modalidades m
+                        Inner Join Provas p on p.codModalidade = m.codModalidade
+                        Inner Join Resultadosatletas r on r.codProva = p.codProva
+                        Inner Join Atletas a on a.codAtleta = r.codAtleta
+                        Order by m.nomeModalidade;", conn
+                    ))
+            using (var rd = cmd.ExecuteReader())
+            {
+                while (rd.Read())
+                {
+                    lista.Add(new Modalidades
+                    {
+                        codModalidade = rd.GetInt32("modalidade"),
+                        nomeModalidade = rd["nome"] as string
+
+                    });
+                }
+            }
+
+            return View(lista);
         }
 
 
