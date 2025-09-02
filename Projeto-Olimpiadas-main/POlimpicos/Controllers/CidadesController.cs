b109a04f583f80a89b50e672e1a348f9e77e468c
Paulo <irineu32323@gmail.com>
2025-09-02 11:01:46 -0300

    5- Fazendo a view index e a controller index no asp.net core

--- Projeto-Olimpiadas-main/POlimpicos/Controllers/CidadesController.cs
+++ Projeto-Olimpiadas-main/POlimpicos/Controllers/CidadesController.cs
@@ -8,10 +8,82 @@ namespace POlimpicos.Controllers
     public class CidadesController : Controller
     {
         private readonly Database db = new Database();
-        public IActionResult Index()
+        public IActionResult Index(int page = 1, int pageSize = 10, string? q = null)
         {
+            page = page < 1 ? 1  : page;
 
-            return View();
+            pageSize = pageSize <= 0 ? 50 : pageSize;
+
+            int totalitems = 0;
+            
+            var itens = new List<Cidade>();
+
+            using(var conn = db.GetConnection())
+            {
+                string where = "";
+                if(!string.IsNullOrWhiteSpace(q))
+                where = "Where c.nomeCidade Like @q Or e.nomeEstado Like @q";
+
+                //Contador
+                using (var cmdCount = new MySqlCommand($@"
+                 Select Count(*) From cidades c
+                 Left Join estados e on c.codEstado = e.codEstado
+                 {where}", conn))
+                {
+                    if (!string.IsNullOrWhiteSpace(q))
+                        cmdCount.Parameters.AddWithValue("@q", "%" + q + "%");
+
+                    totalitems = Convert.ToInt32(cmdCount.ExecuteScalar());
+                }
+
+
+                // Ajuste de página
+                int totalPages = (int)Math.Ceiling(totalitems / (double)pageSize);
+
+                if (totalPages == 0) totalPages = 1;
+                if (page > totalPages) page = totalPages;
+
+                //Pagina
+                // No IFNULL(e.nomeEstado, [Usa aspas simples].
+                using (var cmd = new MySqlCommand($@"
+                Select c.codCidade, c.nomeCidade, IFNULL(e.nomeEstado,'') as nomeEstado 
+                from cidades c
+                Left Join estados e on c.codEstado = e.codEstado
+                {where}
+                Order by c.codCidade
+                Limit @limit OFFSET @offset;", conn))
+                {
+                    if(!string.IsNullOrWhiteSpace(q))
+                    cmd.Parameters.AddWithValue("@q", "%" + q + "%"); 
+                    cmd.Parameters.AddWithValue("@limit", pageSize); 
+                    cmd.Parameters.AddWithValue("@offset", (page - 1) * pageSize);
+
+
+                    using (var r = cmd.ExecuteReader())
+                    {
+                        while(r.Read())
+                        {
+                            itens.Add(new Cidade
+                            {
+                                CodCidade = r.GetInt32("codCidade"),
+                                NomeCidade = r.GetString("nomeCidade"),
+                            });
+                        }
+                    }
+                }
+
+                //*Metadados da paginação via ViewBag
+                // ViewBags podem mandar variavéis ou funções para a página desejada.
+                ViewBag.Page = page;
+                ViewBag.PageSize = pageSize;
+                ViewBag.TotalItems = totalitems;
+                ViewBag.TotalPages = (int)Math.Ceiling(totalitems / (double)pageSize);
+                ViewBag.Query = q;
+               
+            }
+
+
+            return View(itens); //Model = lista simples 
         }
 
         public IActionResult Cadastrar()
